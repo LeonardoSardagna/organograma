8054ce0e83975095763209ec6895b6e93c949dca
Leonardo Sardagna <leonardosardagna00@gmail.com>
2024-03-05 21:25:25 -0300

    formulario finalizado. falta fazer o botao de add

--- src/componentes/formulario/formulario.js
+++ src/componentes/formulario/formulario.js
@@ -1,16 +1,22 @@
 import Campotexto from "../campoTexto/campoTexto"
+import ListaSuspensa from "../listaSuspensa/listaSuspensa"
 import './formulario.css'
 
+const VivoOuMorto = [
+    "Vivo",
+    "Morto"
+]
 
 function Formulario() {
     return (
         <section className="formulario">
             <form>
-                <h1>Lista de personagens do Rick and Morty.</h1>
+                <h1>Lista de personagens do <img className="imagemLogo" src="./imagens/Rick_and_Morty_logo.png"></img> </h1>
                 <Campotexto label="Nome" placeholder="Digite o nome do personagem" />
                 <Campotexto label="Espécie" placeholder="Digite a espécie" />
                 <Campotexto label="Origem" placeholder="Digite a data de origem" />
                 <Campotexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa label= "Vivo ou Morto?" itens={VivoOuMorto}/>
             </form>
         </section>
     )
---
+++ src/componentes/listaSuspensa/listaSuspensa.js
@@ -0,0 +1,13 @@
+import './listaSuspensa.css'
+const ListaSuspensa = (props) =>{
+    return(
+        <div className='listaSuspensa'>
+            <label>{props.label}</label>
+            <select>{props.itens.map(item =>{
+                return <option key={item} >{item}</option>
+            })}</select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
